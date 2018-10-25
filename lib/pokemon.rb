c03439fa7640e758f000f1e7210122c7d8bee163
Brian Birdwell <bbirdwell@gmail.com>
2018-10-25 23:42:59 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -6,7 +6,7 @@ class Pokemon
     @name = name
     @type = type
     @db = db
-    @hp = hp
+    @hp = 60
   end
   
   def self.save(name, type, db)
@@ -16,7 +16,7 @@ class Pokemon
   def self.find(id, db)
     foo = db.execute("SELECT * FROM pokemon WHERE #{id} = id")
     foo.flatten!
-    bar = Pokemon.new(id: foo[0], name: foo[1], type: foo[2], db: @db)
+    bar = Pokemon.new(id: foo[0], name: foo[1], type: foo[2], db: @db, hp: 60)
     # db.execute("INSERT INTO pokemon (hp) VALUES (60)")
     bar
   end
