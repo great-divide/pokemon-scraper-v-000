7a0b30fe024501195d4d37142ca693eff7f313e0
Brian Birdwell <bbirdwell@gmail.com>
2018-10-25 23:52:29 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -16,7 +16,6 @@ class Pokemon
   def self.find(id, db)
     foo = db.execute("SELECT * FROM pokemon WHERE pokemon.id = id")
     foo.flatten!
-    # binding.pry
     bar = Pokemon.new(id: foo[0], name: foo[1], type: foo[2], db: @db, hp: foo[3])
     bar
   end
